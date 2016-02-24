bcc44917ec4732347b33c15617f37d15f89fe6e2
JO JUNGLAE <junglae.jo@daeunextier.com>
2016-02-24 16:08:05 +1100

    fix dependency; implement multiple create;

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
                            [slingshot "0.10.3"]
                            [org.clojure/tools.logging "0.2.3"]
                            [clj-time "0.4.3"]
-                           [com.teamlazerbeez/sf-rest-api-connector "1.0.0-SNAPSHOT"]
-                           [com.teamlazerbeez/sf-soap-api-connector "1.0.0-SNAPSHOT"]])
+                           [com.palominolabs.salesforce/rest-api-connector "1.0.2"]
+                           [com.palominolabs.salesforce/soap-api-connector "1.0.2"]])
